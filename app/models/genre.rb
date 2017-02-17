e878492fe8963eb1096cf74ca09f65a385c2a110
github username <github email address>
2017-02-17 15:46:38 -0500

    done

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,22 @@
+require 'pry'
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
   def song_count
+    self.songs.count
     # return the number of songs in a genre
   end
 
   def artist_count
+    self.artists.count
     # return the number of artists associated with the genre
   end
 
   def all_artist_names
+    arr = [ ]
+     self.artists.each {|artist| arr << artist.name }
+     arr
     # return an array of strings containing every musician's name
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,18 @@
-class Song < ActiveRecord::Base
-  belongs_to :artist
-  belongs_to :genre
 
-  def get_genre_name
-  end
 
-  def drake_made_this
+  class Song < ActiveRecord::Base
+    belongs_to :artist
+    belongs_to :genre
+
+    def get_genre_name
+      self.genre.name
+
+    end
+
+    def drake_made_this
+      drake = Artist.new(name:'Drake')
+
+      self.artist = drake
     # when this method is called it should assign the song's artist to Drake
+    end
   end
-end
